60a4b30a48e42fa19b3dd2fb7270328e6e9d73de
Matheus N <matheusnaschaves@gmail.com>
2023-07-28 19:17:00 -0300

    ws

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -10,9 +10,64 @@ function postText(value){
 
 const scriptsInEvents = {
 
+	async Game_Event61_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event62_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event63_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event64_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event74_Act16(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event75_Act17(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event78_Act5(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:75") 
+	},
+
+	async Menu_Event2_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		
+		try{
+			const webSocket = new WebSocket('wss://arcade.stage.legacyarcade.com/ws', [token,gameId]);
+			runtime.globalVars.webSocket = webSocket;
+			webSocket.onopen = (event) =>{
+				runtime.callFunction('startendless');
+			};
+		}catch(e){
+			const textInstance = runtime.objects.ErrorText.getFirstInstance()
+			textInstance.text = "ERROR CONNECTING"
+			console.log("error connecting to server", e)
+		}
+		
+	},
+
 	async Score_Event3_Act5(runtime, localVars)
 	{
-		postText(runtime.globalVars.postMsg)
+		runtime.globalVars.webSocket.close();
 	}
 
 };
