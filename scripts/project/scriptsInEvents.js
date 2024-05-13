96e27e12a0ad10dae0de4e34aac83873ab7a69b5
tanjounokamioku <matheusnaschaves@gmail.com>
2024-05-13 18:47:23 -0300

    changes from stage

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -10,9 +10,97 @@ function postText(value){
 
 const scriptsInEvents = {
 
+	async Game_Event61_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event62_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event63_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:100") 
+	},
+
+	async Game_Event64_Act6(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event74_Act16(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event75_Act17(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:500") 
+	},
+
+	async Game_Event79_Act5(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:75") 
+	},
+
+	async Game_Event85_Act5(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:75") 
+	},
+
+	async Menu_Event2_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		console.log("clicked")
+					const textInstance = runtime.objects.ErrorText.getFirstInstance()
+					textInstance.text = "PLEASE WAIT..."
+					const button = runtime.objects.play.getFirstInstance();
+					button.destroy(); 
+		// Add a variable to track if the WebSocket is already connected or in the process of connecting
+		let isWebSocketConnectingOrConnected = false; 
+					
+		try {
+		    // Check if the WebSocket is already connected or in the process of connecting
+		    if (!isWebSocketConnectingOrConnected) {
+		        const webSocket = new WebSocket('wss://arcade.legacyarcade.com/ws', [token, gameId]);
+		        runtime.globalVars.webSocket = webSocket;
+		
+		        webSocket.onopen = (event) => {
+		            isWebSocketConnectingOrConnected = true;  // Set the flag to true when connection is established
+		            runtime.callFunction('startendless');
+		        };
+		
+		        webSocket.onclose = (event) => {
+		            isWebSocketConnectingOrConnected = false; // Reset the flag when the connection is closed
+		        };
+		
+		        webSocket.onerror = (event) => {
+		            isWebSocketConnectingOrConnected = false; // Reset the flag on error
+		        };
+		
+		        runtime.globalVars.playable = 1;
+		    }
+		} catch (e) {
+		    const textInstance = runtime.objects.ErrorText.getFirstInstance();
+		    textInstance.text = "ERROR CONNECTING";
+		    console.log("error connecting to server", e);
+		}
+		
+		
+	},
+
 	async Score_Event3_Act5(runtime, localVars)
 	{
-		postText(runtime.globalVars.postMsg)
+		runtime.globalVars.webSocket.close();
+	},
+
+	async Score_Event3_Act6(runtime, localVars)
+	{
+		window.parent.postMessage("WebSocketClosed", "*");
 	}
 
 };
